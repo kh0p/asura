793d976fa8e7c2da161dbcd469c23f55bb5a437b
色とりどりの <yutsuro@gmail.com>
2013-12-01 09:16:46 +0100

    Next variable fix

--- build/asura-build.sh
+++ build/asura-build.sh
@@ -46,7 +46,7 @@ lang=en_US.UTF-8
 def_font=Lat2-Terminus16
 zonetime=Poland
 HOSTNAME="test"
-autofdisk="yes"
+autofdisk=yes
 RAMdisk="mkinitcpio -p linux"
 
 format_color ()
@@ -118,7 +118,7 @@ make_logfile
 
 partition_note
 read -p "Press any key to continue... " -n1 -s
-if [ autofdisk = "yes" ];then
+if [ "$autofdisk" = "yes" ];then
 	sfdisk -d /dev/sda > disk.layout; $cmd_name=sfdisk
 	$success_msg="[$l_green + $default] Done with giving space for $BOOT, $SWAP and $HOME"; std_check
 	echo "Displaying your partition layout (...)"
